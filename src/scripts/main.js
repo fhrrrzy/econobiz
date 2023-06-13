d362625a75e595bd6b96518ecd9bce1223b365ba
sitialiya <sitialiya280@gmail.com>
2023-06-13 21:16:12 +0700

    upload files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,91 +1,17 @@
 import data from '../DATA.json';
-import { productCardTemplate } from './templates/card';
+import { detailProductTemplate } from './templates/card';
 
 const main = () => {
-  const productList = document.querySelector('#container-list');
+  const detailProduct = document.querySelector('#detail-product');
   const dataProduct = data.data.products;
-  const itemsPerPage = 12;
-  let currentPage = 1;
+  detailProduct.innerHTML = '';
 
-  const displayProduct = (data) => {
-    productList.innerHTML = '';
-    data.forEach((product) => {
-      productList.innerHTML += productCardTemplate(product);
-    });
-  };
-
-  const renderPagination = () => {
-    const totalItems = dataProduct.length;
-    const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-    const pageNumbersContainer = document.querySelector('#page-number');
-    pageNumbersContainer.innerHTML = '';
-
-    for (let i = 1; i <= totalPages; i++) {
-      const pageNumber = document.createElement('div');
-      pageNumber.classList.add('page-number-item', 'rounded-full', 'flex', 'p-2', 'mx-1', 'w-10', 'h-10', 'justify-center', 'items-center', 'md:p-3', 'lg:p-4');
-      pageNumber.textContent = i;
-
-      if (i === currentPage) {
-        pageNumber.classList.add('active', 'bg-greenbase', 'text-white');
-      }
-
-      pageNumber.addEventListener('click', () => {
-        currentPage = i;
-        renderPagination();
-        renderProductList();
-      });
-
-      pageNumbersContainer.appendChild(pageNumber);
-    }
-
-    const prevButton = document.querySelector('#prev button');
-    const nextButton = document.querySelector('#next button');
-
-    if (currentPage === 1) {
-      prevButton.disabled = true;
-    } else {
-      prevButton.disabled = false;
-    }
-
-    if (currentPage === totalPages) {
-      nextButton.disabled = true;
-    } else {
-      nextButton.disabled = false;
-    }
-  };
-
-  const renderProductList = () => {
-    const start = (currentPage - 1) * itemsPerPage;
-    const end = start + itemsPerPage;
-    const displayedData = dataProduct.slice(start, end);
-    displayProduct(displayedData);
-  };
-
-  const prevButton = document.querySelector('#prev');
-  const nextButton = document.querySelector('#next');
-
-  prevButton.addEventListener('click', () => {
-    if (currentPage > 1) {
-      currentPage--;
-      renderPagination();
-      renderProductList();
-    }
+  dataProduct.forEach((product) => {
+    const updatedProduct = { ...product };
+    const descriptionText = updatedProduct.potentials.replace(/\n/g, '<br>');
+    updatedProduct.potentials = descriptionText;
+    detailProduct.innerHTML += detailProductTemplate(updatedProduct);
   });
-
-  nextButton.addEventListener('click', () => {
-    const totalItems = dataProduct.length;
-    const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-    if (currentPage < totalPages) {
-      currentPage++;
-      renderPagination();
-      renderProductList();
-    }
-  });
-
-  renderPagination();
-  renderProductList();
 };
 
 export default main;
--- src/scripts/templates/card.js
+++ src/scripts/templates/card.js
@@ -1,27 +1,36 @@
-const productCardTemplate = (product) => `
-  <div class="aspect=[4/3] rounded-3xl relative bg-greenbgimage text-white">
-    <div class="absolute top-0 p-2 lg:p-3 z-10">
-        <a href="#" class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 lg:w-[44px] lg:h-[44px]">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-            </svg>
-            <div class="ml-2 capitalize">
-                <p class="text-lg lg:text-xl">${product.user_name}</p>
-                <p class="text-sm lg:text-base">${product.province}</p>
-            </div>
-        </a>
-    </div>
-    <a href="#">
-        <img src="${product.photo}" alt="background" class="rounded-3xl w-full filter brightness-50">
-        <div class="absolute top-0 p-2 right-0 lg:p-3 normal-case">
-            <p class="bg-slate-900 rounded-full px-4 py-2 lg:text-lg">${product.category}</p>
-        </div>
-        <div class="absolute p-4 bottom-0 lg:[18px] capitalize">
-            <h2 class="font-bold text-4xl lg:text-[40px]">${product.name}</h2>
-            <p class="font-semibold text-xl lg:text-[22px]">Rp ${product.price}</p>
+const detailProductTemplate = (product) => `
+<div class="relative">
+<img src="${product.photo}" alt="${product.name}" class="rounded-3xl w-full filter brightness-90">
+<p class="bg-slate-300 rounded-tr-3xl rounded-bl-3xl p-4 py-2 absolute top-0 right-0 lg:p-3 normal-case lg:text-lg">${product.category}</p>
+<h2 class="font-bold text-2xl px-4 pt-4 capitalize md:text-[26px]">${product.name}</h2>
+<p class="font-semibold text-3xl px-4 pb-2 md:text-[34px]">Rp ${product.price}</p>
+
+<div class="p-4 capitalize flex items-center justify-between">
+    <a href="#" class="flex items-center">
+        <img src="https://source.unsplash.com/400x400?people" alt="profil" class="rounded-full h-12 w-12">
+        <div class="ml-2 capitalize">
+            <p class="text-lg md:text-xl">${product.user_name}</p>
+            <p class="text-sm md:text-base">${product.province}</p>
+            <p class="text-xs md:text-sm">Joined ${product.join_date}</p>
         </div>
     </a>
-  </div>
+    <button class="bg-greenbase rounded-full p-2 shadow-lg md:p-3 md: text-lg">Hubungi Penjual</button>
+</div>        
+</div>
+
+<div class="px-4 md:text-lg">
+<div class="flex items-center py-2 capitalize font-bold">
+    <h2>Deskripsi</h2>
+    <div class="border-r-[8rem] border-black h-1 ml-4 lg:border-r-[12rem]"></div>
+</div>
+<p class="text-justify pb-2">${product.description}</p>
+
+<div class="flex items-center py-2 capitalize font-bold">
+    <h2>Potensi penggunaan/pengolahan lebih lanjut</h2>
+    <div class="border-r-[8rem] border-black h-1 ml-4 lg:border-r-[12rem]"></div>
+</div>
+<p class="text-justify pb-2">${product.potentials}</p>
+</div>
 `;
 
-export { productCardTemplate };
+export { detailProductTemplate };
