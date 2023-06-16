a77149f5e673f98f1bddc58a5e7b006781aa5d13
sitialiya <sitialiya280@gmail.com>
2023-06-16 22:22:57 +0700

    update review card

--- src/scripts/templates/card.js
+++ src/scripts/templates/card.js
@@ -24,4 +24,30 @@ const productCardTemplate = (product) => `
   </div>
 `;
 
-export { productCardTemplate };
+const ratingsElement = (rating) => {
+  const filledStar = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block stroke-[1px] stroke-yellow-300 fill-yellow-300"> <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" /> </svg>';
+  const emptyStar = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block stroke-[1px] stroke-yellow-300"> <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" /> </svg>';
+
+  const ratingStars = filledStar.repeat(rating) + emptyStar.repeat(5 - rating);
+
+  return ratingStars;
+};
+
+const feedbackCardTemplate = (feedback) => `
+    <img src="./image/quote.png" alt="quote" class="absolute -top-0 right-0">
+    <div class="flex flex-row gap-5 mb-4">
+    <img src="./image/profile.jpg" alt="profile reviewer" class="rounded-full w-14 h-14">
+    <div>
+        <p class="break-words text-md font-sans font-semibold mb-1 max-w-[10rem]">${feedback.user_name}</p>
+        <div class="star">${ratingsElement(feedback.rating)}</div>
+    </div>
+    </div>
+    <div class="text-justify">
+    <q class="text-slate-700 font-sans break-all text-sm">${feedback.review}</q>
+    </div>
+`;
+
+export {
+  productCardTemplate,
+  feedbackCardTemplate,
+};
