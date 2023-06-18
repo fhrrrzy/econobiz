69264d9652ac8651c7b781a75753e811e7f50ecc
Fahrurozi <fahrurrozy4214@gmail.com>
2023-06-19 00:43:28 +0700

    Add product (#11)
    * update package.json
    
    * update review card
    
    * merging
    
    * render product and feedback from API
    
    * update main.js
    
    ---------
    
    Co-authored-by: sitialiya <sitialiya280@gmail.com>

--- src/scripts/templates/card.js
+++ src/scripts/templates/card.js
@@ -6,7 +6,7 @@ const productCardTemplate = (product) => `
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
             <div class="ml-1 capitalize">
-                <p class="text-md md: lg:text-lg">${product.user_name}</p>
+                <p class="text-md md: lg:text-lg">${product.umkm_name}</p>
                 <p class="text-sm lg:text-base">${product.province}</p>
             </div>
         </a>
@@ -18,7 +18,7 @@ const productCardTemplate = (product) => `
         </div>
         <div class="absolute p-4 bottom-0 lg:[18px] capitalize">
             <h2 class="font-bold text-3xl lg:text-[32px]">${product.name}</h2>
-            <p class="font-semibold text-lg lg:text-xl">Rp ${product.price.toLocaleString('id-ID')}</p>
+            <p class="font-semibold text-lg lg:text-xl">Rp ${Number(product.price).toLocaleString('id-ID')}</p>
         </div>
     </a>
   </div>
