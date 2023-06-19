9c1ed685d9b7217f8f97f0eaa3e0d6cf2bdeb6ce
fhrrrzy <fahrurrozy4214@gmail.com>
2023-06-19 14:20:57 +0700

    minor fix styling

--- src/scripts/templates/card.js
+++ src/scripts/templates/card.js
@@ -1,12 +1,12 @@
 const productCardTemplate = (product) => `
-  <div class="aspect-4/3 rounded-3xl relative bg-greenbgimage text-white">
+  <div class="aspect-square rounded-3xl w-full relative bg-greenbgimage text-white">
     <div class="absolute top-0 p-2 lg:p-3 z-10">
         <a href="#" id="profile" class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 lg:w-[36px] lg:h-[36px]">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
             <div class="ml-1 capitalize">
-                <p class="text-md md: lg:text-lg">${product.umkm_name}</p>
+                <p class="text-sm lg:text-lg mb-0">${product.umkm_name}</p>
                 <p class="text-sm lg:text-base">${product.province}</p>
             </div>
         </a>
@@ -16,8 +16,8 @@ const productCardTemplate = (product) => `
             <img src="${product.photo}" alt="background" class="rounded-3xl object-cover w-full h-full filter brightness-50">
         </div>
         <div class="absolute p-4 bottom-0 lg:[18px] capitalize">
-            <h2 class="font-bold text-3xl lg:text-[32px]">${product.name}</h2>
-            <p class="font-semibold text-lg lg:text-xl">Rp ${Number(product.price).toLocaleString('id-ID')}</p>
+            <h2 class="font-semibold text-xl lg:text-2xl">${product.name}</h2>
+            <p class="font-medium text-lg lg:text-xl">Rp ${Number(product.price).toLocaleString('id-ID')}</p>
         </div>
     </a>
   </div>
